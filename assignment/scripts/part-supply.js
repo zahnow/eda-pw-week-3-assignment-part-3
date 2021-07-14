67897d4c2ad9354fb75d76b0c19ba63da1731974
Edan Schwartz <edanschwartz@gmail.com>
2021-07-14 16:29:42 -0500

    Tweak working around instructions

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,40 +5,45 @@ console.log('****** Part Supply *******');
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
 
-// 2. Create a variable call 'supplyChanges' set it to an array containing
+// 2. Our company builds subterrainian roller-coasters, and we need an app
+//    to track our orders for roller-coaster parts.
+//    Create a variable call 'partOrders' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+console.log('2. Array of partOrders:');
 
-// 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+// 3. Console log the value of the second item in the 'partOrders' array
+console.log('3. Second partOrder is:');
 
-// 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
+// 4. The last item was added by mistake. Remove it from the 'partOrders' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log('5. Adding 25 to supplyChanges.');
+console.log('5. Adding 25 to partOrders.');
 
-// 6. Write a `for` loop that shows each value in the 'supplyChanges' array
+// 6. Write a `for` loop that shows each value in the 'partOrders' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
-console.log('6. Showing supplyChanges...');
+console.log('6. Showing partOrders...');
 
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
-console.log('7. Showing supplyChanges with "for of" loop');
+console.log('7. Showing partOrders with "for of" loop');
 
 // 8. Business is booming! We're doubling our orders for parts every week.
 // Write a `while` loop that adds twice the number of parts as was in the 
 // previous order. Keep adding orders until we've passed 100 parts in an order
 // -- then we'll need to consider getting a bigger loading dock!
-console.log('8. Showing supplyChanges with "while" loop');
+console.log('8. Showing partOrders with "while" loop');
 
 
-// 9. Write a loop to determine the total number of parts available by
-//    adding up all the numbers in the 'supplyChanges' array.
-console.log('9. Total supplies available is:');
+// 9. We're getting ready to build the largest subterrainian roller coaster ever!
+//    We need to take stock of all our roller coaster parts, to see how 
+//    big we can make it.
+//    Write a loop to determine the total number of parts available by
+//    adding up all the numbers in the 'partOrders' array.
+console.log('9. Total parts available is:');
